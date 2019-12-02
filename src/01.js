b37734595089371d73d2b5187c0e73148b92a781
Melvin Vermeer <melvin.vermeer@nike.com>
2019-12-02 22:24:45 +0100

    implemant day 1 part two, the double checker

--- src/01.js
+++ src/01.js
@@ -2,13 +2,34 @@
 // Specifically, to find the fuel required for a module, take its mass,
 // divide by three, round down, and subtract 2.
 
+// Part 2
+// Fuel itself requires fuel just like a module - take its mass,
+// divide by three, round down, and subtract 2. However, that
+// fuel also requires fuel, and that fuel requires fuel, and so on.
+// Any mass that would require negative fuel should instead be treated
+// as if it requires zero fuel; the remaining mass, if any, is instead handled
+// by wishing really hard, which has no mass and is outside the scope of this calculation.
+
 const calculateFuel = (mass) =>
-  Math.floor(mass / 3) - 2;
+  Math.max(Math.floor(mass / 3) - 2, 0);
+
+const calculateFuelPlusExtra = (mass) => {
+  const moduleFuel = calculateFuel(mass);
+  let totalFuel = moduleFuel;
+  let extraFuel = moduleFuel;
+
+  do {
+    extraFuel = calculateFuel(extraFuel);
+    totalFuel += extraFuel;
+  } while (extraFuel > 0);
+
+  return totalFuel;
+};
 
 const calculateFuelForMultipleModules = (moduleMasses) =>
-  moduleMasses.map(calculateFuel).reduce((a, b) => a + b);
+  moduleMasses.map(calculateFuelPlusExtra).reduce((a, b) => a + b);
 
 module.exports = {
-  calculateFuel,
+  calculateFuelPlusExtra,
   calculateFuelForMultipleModules,
 };
--- tst/01.test.js
+++ tst/01.test.js
@@ -1,28 +1,20 @@
-const { calculateFuel, calculateFuelForMultipleModules } = require('../src/01');
+const { calculateFuelPlusExtra, calculateFuelForMultipleModules } = require('../src/01');
 const data = require('./01.data');
 
 describe('01 - The Tyranny of the Rocket Equation', () => {
-  it('12 -> 2', () => {
-    expect(calculateFuel(12)).toEqual(2);
+  it('14 -> 2', () => {
+    expect(calculateFuelPlusExtra(8)).toEqual(0);
   });
 
-  it('14 -> 2', () => {
-    expect(calculateFuel(14)).toEqual(2);
+  it('100756 -> 50346', () => {
+    expect(calculateFuelPlusExtra(100756)).toEqual(50346);
   });
 
   it('1969 -> 654', () => {
-    expect(calculateFuel(1969)).toEqual(654);
+    expect(calculateFuelPlusExtra(1969)).toEqual(966);
   });
 
-  it('100756 -> 33583', () => {
-    expect(calculateFuel(100756)).toEqual(33583);
-  });
-
-  it('[12,14] -> 4', () => {
-    expect(calculateFuelForMultipleModules([12, 14])).toEqual(4);
-  });
-
-  it('aswer -> 3332538', () => {
-    expect(calculateFuelForMultipleModules(data)).toEqual(3332538);
+  it('aswer -> 4995942', () => {
+    expect(calculateFuelForMultipleModules(data)).toEqual(4995942);
   });
 });
