4c9fcd60dc8867b22e5abbc9dc4c90a5310fff1a
Melvin Vermeer <melvin.vermeer@nike.com>
2019-12-13 21:12:12 +0100

    duncan

--- src/01-the-tyranny-of-the-rocket-equation.js
+++ src/01-the-tyranny-of-the-rocket-equation.js
@@ -1,33 +1,19 @@
-// Part 1
-// Fuel required to launch a given module is based on its mass.
-// Specifically, to find the fuel required for a module, take its mass,
-// divide by three, round down, and subtract 2.
+const calculateFuel = (mass) => {
+  let totalFuel = 0;
 
-// Part 2
-// Fuel itself requires fuel just like a module - take its mass,
-// divide by three, round down, and subtract 2. However, that
-// fuel also requires fuel, and that fuel requires fuel, and so on.
-// Any mass that would require negative fuel should instead be treated
-// as if it requires zero fuel; the remaining mass, if any, is instead handled
-// by wishing really hard, which has no mass and is outside the scope of this calculation.
-
-const sum = (a, b) => a + b;
-
-const calculateFuel = (mass) =>
-  Math.max(Math.floor(mass / 3) - 2, 0);
-
-const calculateFuelPlusExtra = (mass) => {
-  const fuel = calculateFuel(mass);
-  if (fuel > 0) {
-    return fuel + calculateFuelPlusExtra(fuel);
+  let fuelNeeded = Math.floor(mass / 3) - 2;
+  if (fuelNeeded < 0) {
+    fuelNeeded = 0;
   }
-  return 0;
+  totalFuel += fuelNeeded;
+  if (fuelNeeded > 0) {
+    const extraMass = calculateFuel(fuelNeeded);
+    totalFuel += extraMass;
+  }
+
+  return totalFuel;
 };
 
-const calculateFuelForMultipleModules = (masses) =>
-  masses.map(calculateFuelPlusExtra).reduce(sum);
-
 module.exports = {
-  calculateFuelPlusExtra,
-  calculateFuelForMultipleModules,
+  calculateFuel,
 };
--- tst/01.test.js
+++ tst/01.test.js
@@ -1,20 +1,22 @@
-const { calculateFuelPlusExtra, calculateFuelForMultipleModules } = require('../src/01-the-tyranny-of-the-rocket-equation');
-const data = require('./01.data');
+const { calculateFuel } = require('../src/01-the-tyranny-of-the-rocket-equation');
 
 describe('01 - The Tyranny of the Rocket Equation', () => {
+  it('12 -> 2', () => {
+    expect(calculateFuel(12)).toEqual(2);
+  });
   it('14 -> 2', () => {
-    expect(calculateFuelPlusExtra(8)).toEqual(0);
+    expect(calculateFuel(14)).toEqual(2);
   });
-
+  it('28 -> 7', () => {
+    expect(calculateFuel(28)).toEqual(7);
+  });
+  it('60 -> 22', () => {
+    expect(calculateFuel(60)).toEqual(22);
+  });
+  it('1969 -> 966', () => {
+    expect(calculateFuel(1969)).toEqual(966);
+  });
   it('100756 -> 50346', () => {
-    expect(calculateFuelPlusExtra(100756)).toEqual(50346);
-  });
-
-  it('1969 -> 654', () => {
-    expect(calculateFuelPlusExtra(1969)).toEqual(966);
-  });
-
-  it('aswer -> 4995942', () => {
-    expect(calculateFuelForMultipleModules(data)).toEqual(4995942);
+    expect(calculateFuel(100756)).toEqual(50346);
   });
 });
